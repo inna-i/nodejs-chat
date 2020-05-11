1f864472dd39b40792c7934bd64ba11f4d54f7f7
inna-i <iivashchuk@talend.com>
2020-05-11 14:23:37 +0300

    feat: lint and travic

--- src/Chat/index.js
+++ src/Chat/index.js
@@ -1,156 +1,156 @@
-import React, { useState, useEffect, useRef } from "react";
-import styled from "styled-components";
-import socketIOClient from "socket.io-client";
+import React, { useState, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import socketIOClient from 'socket.io-client';
 
-import MessagesList from "../MessagesList";
+import MessagesList from '../MessagesList';
 
 const H1 = styled.h1`
-  position: relative;
-  text-align: center;
-  letter-spacing: 10px;
+	position: relative;
+	text-align: center;
+	letter-spacing: 10px;
 `;
 
 const Status = styled.div`
-  position: fixed;
-  right: 20px;
-  top: 20px;
-  padding: 3px 10px;
-  border-radius: 5px;
-  font-size: 13px;
-  background: ${(props) => (props.status === "online" ? "#6fb472" : "#F06292")};
+	position: fixed;
+	right: 20px;
+	top: 20px;
+	padding: 3px 10px;
+	border-radius: 5px;
+	font-size: 13px;
+	background: ${props => (props.status === 'online' ? '#6fb472' : '#F06292')};
 `;
 
 const ChatContainer = styled.div`
-  color: #fbfbef;
-  font-family: "Consolas", monospace;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
+	color: #fbfbef;
+	font-family: "Consolas", monospace;
+	display: flex;
+	flex-direction: column;
+	height: 100vh;
 `;
 
 const Form = styled.form`
-  position: relative;
-  margin: 20px 30px;
+	position: relative;
+	margin: 20px 30px;
 
-  &::before {
-    content: "~msg$:";
-    position: absolute;
-    left: 14px;
-    top: 11px;
-    color: #dcd1c4;
-    font-size: 16px;
-  }
+	&::before {
+		content: "~msg$:";
+		position: absolute;
+		left: 14px;
+		top: 11px;
+		color: #dcd1c4;
+		font-size: 16px;
+	}
 
-  &::after {
-    content: "↵";
-    position: absolute;
-    right: 20px;
-    top: 0px;
-    color: #dcd1c4;
-    font-size: 30px;
-  }
+	&::after {
+		content: "↵";
+		position: absolute;
+		right: 20px;
+		top: 0px;
+		color: #dcd1c4;
+		font-size: 30px;
+	}
 `;
 
 const Input = styled.input`
-  outline: none;
-  border: none;
-  background-color: #3c4556;
-  border-radius: 5px;
-  color: #ece7dc;
-  width: 100%;
-  height: 40px;
-  text-indent: 82px;
-  display: block;
-  font-size: 16px;
-  font-family: "Consolas", monospace;
-  flex: 1;
+	outline: none;
+	border: none;
+	background-color: #3c4556;
+	border-radius: 5px;
+	color: #ece7dc;
+	width: 100%;
+	height: 40px;
+	text-indent: 82px;
+	display: block;
+	font-size: 16px;
+	font-family: "Consolas", monospace;
+	flex: 1;
 `;
 
 function hostName(meta) {
 	if (meta && meta.HOSTNAME) {
-		return meta.HOSTNAME
+		return meta.HOSTNAME;
 	}
-	return 'default'
+	return 'default';
 }
 
 const Chat = () => {
-  const [messages, setMessages] = useState([]);
-  const [value, setValue] = useState("");
-  const [io, setIo] = useState(null);
-  const [userId, setUserId] = useState(null);
-  const [status, setStatus] = useState("offline");
-  const [activeUsers, setActiveUsers] = useState([]);
-  const el = useRef(null);
-  const [meta, setMeta] = useState({});
+	const [messages, setMessages] = useState([]);
+	const [value, setValue] = useState('');
+	const [io, setIo] = useState(null);
+	const [userId, setUserId] = useState(null);
+	const [status, setStatus] = useState('offline');
+	const [activeUsers, setActiveUsers] = useState([]);
+	const el = useRef(null);
+	const [meta, setMeta] = useState({});
 
-  useEffect(() => {
-    fetch("/api")
-      .then((resp) => resp.json())
-      .then((resp) => setMeta(resp));
+	useEffect(() => {
+		fetch('/api')
+			.then(resp => resp.json())
+			.then(resp => setMeta(resp));
 
-    const chat = socketIOClient("/api/chat");
+		const chat = socketIOClient('/api/chat');
 
-    setIo(chat);
+		setIo(chat);
 
-    chat.on("connect", () => {
-      console.log("connected");
-      setStatus("online");
-    });
+		chat.on('connect', () => {
+			console.info('connected');
+			setStatus('online');
+		});
 
-    chat.on("userConnected", (user) => {
-      setActiveUsers((u) => u.concat(user));
-    });
+		chat.on('userConnected', user => {
+			setActiveUsers(u => u.concat(user));
+		});
 
-    chat.on("userDisconnected", (user) => {
-      setActiveUsers((u) => u.filter((name) => name !== user));
-    });
+		chat.on('userDisconnected', user => {
+			setActiveUsers(u => u.filter(name => name !== user));
+		});
 
-    chat.on("activeUsers", (users) => {
-      console.log('activeUsers', users);
-      setActiveUsers((u) => Array.from(users));
-    });
+		chat.on('activeUsers', users => {
+			console.info('activeUsers', users);
+			setActiveUsers(() => Array.from(users));
+		});
 
-    chat.on("message", (m) => {
-      if (m.currUserId) {
-        setUserId(m.currUserId);
-      }
+		chat.on('message', m => {
+			if (m.currUserId) {
+				setUserId(m.currUserId);
+			}
 
-      setMessages((d) => d.concat(m));
-      if (el.current) {
-        el.current.scrollTop = el.current.scrollHeight;
-      }
-    });
+			setMessages(d => d.concat(m));
+			if (el.current) {
+				el.current.scrollTop = el.current.scrollHeight;
+			}
+		});
 
-    return () => chat.disconnect();
-  }, []);
+		return () => chat.disconnect();
+	}, []);
 
-  return (
-    <ChatContainer>
-      <Status status={status}>{hostName(meta)}</Status>
-      <H1>NWSD</H1>
-      <MessagesList
-        el={el}
-        messages={messages}
-        userId={userId}
-        activeUsers={activeUsers}
-      />
-      <Form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (value !== "") {
-            io.emit("message", value);
-            setValue("");
-          }
-        }}
-      >
-        <Input
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          placeholder="Enter your message"
-        />
-      </Form>
-    </ChatContainer>
-  );
+	return (
+		<ChatContainer>
+			<Status status={status}>{hostName(meta)}</Status>
+			<H1>NWSD</H1>
+			<MessagesList
+				el={el}
+				messages={messages}
+				userId={userId}
+				activeUsers={activeUsers}
+			/>
+			<Form
+				onSubmit={e => {
+					e.preventDefault();
+					if (value !== '') {
+						io.emit('message', value);
+						setValue('');
+					}
+				}}
+			>
+				<Input
+					value={value}
+					onChange={e => setValue(e.target.value)}
+					placeholder="Enter your message"
+				/>
+			</Form>
+		</ChatContainer>
+	);
 };
 
 export default Chat;
--- src/Info/index.js
+++ src/Info/index.js
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const Ping = () => {
-  const [meta, setMeta] = useState({});
+	const [meta, setMeta] = useState({});
 
-  useEffect(() => {
-    fetch("/api")
-      .then((resp) => resp.json())
-      .then((resp) => setMeta(resp))
-      .catch((e) => alert("fail"));
-  }, []);
+	useEffect(() => {
+		fetch('/api')
+			.then(resp => resp.json())
+			.then(resp => setMeta(resp))
+			.catch(() => console.error('fail'));
+	}, []);
 
-  return (
-    <div style={{ display: "flex", color: "#ece7dc" }}>
-      <div style={{ color: "grey", maxWidth: 500 }}>
-        {Object.keys(meta).map((key) => (
-          <div>
-            {key}: {meta[key]}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div style={{ display: 'flex', color: '#ece7dc' }}>
+			<div style={{ color: 'grey', maxWidth: 500 }}>
+				{Object.keys(meta).map(key => (
+					<div>
+						{key}: {meta[key]}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Ping;
